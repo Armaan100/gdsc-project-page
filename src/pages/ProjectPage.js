b9f4302c038df5cff3b57d0932d6db108dc50ed5
Armaan Gogoi <gogoiarmaan5@gmail.com>
2024-08-11 17:58:18 +0530

    added skeleton

---
+++ src/pages/ProjectPage.js
@@ -0,0 +1,10 @@
+import React from "react"
+import MainContent from "../components/MainContent";
+
+const ProjectPage = () => {
+    return(
+        <MainContent />
+    )
+}
+
+export default ProjectPage;
